207b9946c2b59ff880c596413f867f85c10297d7
MASHOD0 <mashhood2002@gmail.com>
2021-01-27 23:27:48 +0530

    made tokenize function in loadData.py

--- src/data/loadData.py
+++ src/data/loadData.py
@@ -20,3 +20,29 @@ def load_files(directory):
     return info
 
 
+def tokenize(document):
+    """
+    Given a document (represented as a string), return a list of all of the
+    words in that document, in order.
+
+    Process document by coverting all words to lowercase, and removing any
+    punctuation or English stopwords.
+    """
+    final_words = []
+    avoided_words = [] # WORDS WHICH ARE TO BE AVOIDED IN THE FINAL LIST
+    
+    # making the avoided_words list
+    for word in string.punctuation: # the string library has a string of punctuations
+        avoided_words.append(word)
+    for word in nltk.corpus.stopwords.words("english"): # the nltk lib. has a list of stopwords commonly used in english
+        avoided_words.append(word)
+
+    tokens = nltk.word_tokenize(document)
+
+    # filtering the tokens
+    for token in tokens :
+        if token not in avoided_words:
+            final_words.append(token.lower())
+
+    return final_words
+    
