4639258e0f6e3933b35bf61570dadfb72387bc42
MASHOD0 <mashhood2002@gmail.com>
2021-01-27 23:38:32 +0530

    made top files func

--- src/tfidf/tfidf.py
+++ src/tfidf/tfidf.py
@@ -25,3 +25,31 @@ def compute_idfs(documents):
     # calculating the idf value for indivisual words
     for word, value in counts.items():
         idfs[word] = math.log( (num_docs / value) )
+
+
+def tokenize(document):
+    """
+    Given a document (represented as a string), return a list of all of the
+    words in that document, in order.
+
+    Process document by coverting all words to lowercase, and removing any
+    punctuation or English stopwords.
+    """
+    final_words = []
+    avoided_words = [] # WORDS WHICH ARE TO BE AVOIDED IN THE FINAL LIST
+    
+    # making the avoided_words list
+    for word in string.punctuation: # the string library has a string of punctuations
+        avoided_words.append(word)
+    for word in nltk.corpus.stopwords.words("english"): # the nltk lib. has a list of stopwords commonly used in english
+        avoided_words.append(word)
+
+    tokens = nltk.word_tokenize(document)
+
+    # filtering the tokens
+    for token in tokens :
+        if token not in avoided_words:
+            final_words.append(token.lower())
+
+    return final_words
+    
