c3cc9318a2fc7020ef4d858c2897b9aa5a4506df
MASHOD0 <mashhood2002@gmail.com>
2021-01-29 19:49:54 +0530

    bug fix in main.py

--- src/main.py
+++ src/main.py
@@ -43,7 +43,7 @@ def main():
     idfs = tfidf.compute_idfs(sentences)
 
     # Determine top sentence matches
-    matches = tfidf.top_sentences(query, sentences, idfs, n=SENTENCE_MATCHES)
+    matches = tfidf.top_sentences(query, sentences, idfs, n=SENTENCES)
     for match in matches:
         print(match)
 
