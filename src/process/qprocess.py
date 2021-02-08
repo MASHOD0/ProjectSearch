c2c55620202da0835b0dc2d5742f3ecc43face5e
MASHOD0 <mashhood2002@gmail.com>
2021-02-08 23:43:44 +0530

    fixed a bug

--- src/process/qprocess.py
+++ src/process/qprocess.py
@@ -6,4 +6,4 @@ def filter(question):
     for word in question:
         filtered_words = ["how","what"]
         if word in filtered_words:
-            question.pop(word)
+            question.remove(word)
