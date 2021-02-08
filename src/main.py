b7daef71a814e12f21533bcee09585d5bf5c92c3
MASHOD0 <mashhood2002@gmail.com>
2021-02-08 23:41:14 +0530

    bug fix

--- src/main.py
+++ src/main.py
@@ -10,9 +10,9 @@ FILE = 4
 SENTENCES = 4
 
 def main():
-"""
-This is the main function it links all the other functions to run the program.
-"""    
+    """
+    This is the main function it links all the other functions to run the program.
+    """    
     # Check command-line arguments
     if len(sys.argv) != 2:
         sys.exit("Usage: python src/main.py corpus")
