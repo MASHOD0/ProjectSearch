868b7e004e9159213fda733656cc08cb90a8a7f2
MASHOD0 <mashhood2002@gmail.com>
2021-02-26 20:30:06 +0530

    made made tokenise function

--- src/process/qprocess.py
+++ src/process/qprocess.py
@@ -9,3 +9,23 @@ def filter(question):
         filtered_words = ["how","what"]
         if word in filtered_words:
             question.remove(word)
+def tokenize(document):
+    """
+    Given a document (represented as a string), return a list of all of the
+    words in that document, in order.
+
+    Process document by coverting all words to lowercase, and removing any
+    punctuation or English stopwords.
+    :param document: document to tokenize
+    :return: tokenized list
+    """
+    final_words = []
+    avoided_words = [] # WORDS WHICH ARE TO BE AVOIDED IN THE FINAL LIST
+    
+    # making the avoided_words list
+    for word in string.punctuation: # the string library has a string of punctuations
+        avoided_words.append(word)
+    for word in nltk.corpus.stopwords.words("english"): # the nltk lib. has a list of stopwords commonly used in english
+        avoided_words.append(word)
+
+    tokens = nltk.word_tokenize(document)
